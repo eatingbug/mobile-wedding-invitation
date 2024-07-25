1d0836c5cfdeb106ba1f92172dfb5fec4dab96c2
eatingbug <tmd5972@gmail.com>
2024-07-25 21:32:39 +0900

    Update gallery

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -1,15 +1,29 @@
-import image01 from '@/assets/images/01.jpg'
-import image02 from '@/assets/images/02.jpg'
-import image03 from '@/assets/images/03.jpg'
-import image04 from '@/assets/images/04.jpg'
-import image05 from '@/assets/images/05.jpg'
-import image06 from '@/assets/images/06.jpg'
-import image07 from '@/assets/images/07.jpg'
-// import image08 from '@/assets/images/08.jpg'
-// import image09 from '@/assets/images/09.jpg'
-// import image10 from '@/assets/images/10.jpg'
-// import image11 from '@/assets/images/11.jpg'
-// import image12 from '@/assets/images/12.jpg'
+import image01 from '@/assets/images/01.jpeg'
+import image02 from '@/assets/images/02.jpeg'
+import image03 from '@/assets/images/03.jpeg'
+import image04 from '@/assets/images/04.jpeg'
+import image05 from '@/assets/images/05.jpeg'
+import image06 from '@/assets/images/06.jpeg'
+import image07 from '@/assets/images/07.jpeg'
+import image08 from '@/assets/images/08.jpeg'
+import image09 from '@/assets/images/09.jpeg'
+import image10 from '@/assets/images/10.jpeg'
+import image11 from '@/assets/images/11.jpeg'
+import image12 from '@/assets/images/12.jpeg'
+import image13 from '@/assets/images/13.jpeg'
+import image14 from '@/assets/images/14.jpeg'
+import image15 from '@/assets/images/15.jpeg'
+import image16 from '@/assets/images/16.jpeg'
+import image17 from '@/assets/images/17.jpeg'
+import image18 from '@/assets/images/18.jpeg'
+import image19 from '@/assets/images/19.jpeg'
+import image20 from '@/assets/images/20.jpeg'
+import image21 from '@/assets/images/21.jpeg'
+import image22 from '@/assets/images/22.jpeg'
+import image23 from '@/assets/images/23.jpeg'
+import image24 from '@/assets/images/24.jpeg'
+import image25 from '@/assets/images/25.jpeg'
+import image26 from '@/assets/images/26.jpeg'
 
 const images = [   /* 이미지 경로를 리스트로 저장 */
   {
@@ -35,21 +49,65 @@ const images = [   /* 이미지 경로를 리스트로 저장 */
   },{
     alt: 'image07',
     source: image07,
-  // },{
-  //   alt: 'image08',
-  //   source: image08,
-  // },{
-  //   alt: 'image09',
-  //   source: image09,
-  // },{
-  //   alt: 'image10',
-  //   source: image10,
-  // },{
-  //   alt: 'image11',
-  //   source: image11,
-  // },{
-  //   alt: 'image12',
-  //   source: image12,
+  },{
+    alt: 'image08',
+    source: image08,
+  },{
+    alt: 'image09',
+    source: image09,
+  },{
+    alt: 'image10',
+    source: image10,
+  },{
+    alt: 'image11',
+    source: image11,
+  },{
+    alt: 'image12',
+    source: image12,
+  },{
+    alt: 'image13',
+    source: image13,
+  },
+  {
+    alt: 'image14',
+    source: image14,
+  },
+  {
+    alt: 'image15',
+    source: image15,
+  },{
+    alt: 'image16',
+    source: image16,
+  },{
+    alt: 'image17',
+    source: image17,
+  },{
+    alt: 'image18',
+    source: image18,
+  },{
+    alt: 'image19',
+    source: image19,
+  },{
+    alt: 'image20',
+    source: image20,
+  },{
+    alt: 'image21',
+    source: image21,
+  },{
+    alt: 'image22',
+    source: image22,
+  },{
+    alt: 'image23',
+    source: image23,
+  },{
+    alt: 'image24',
+    source: image24,
+  },{
+    alt: 'image25',
+    source: image25,
+  },{
+    alt: 'image26',
+    source: image26,
   }
 ];
 
